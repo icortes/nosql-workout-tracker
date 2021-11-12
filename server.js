a79fec4743b2a975f09d7978229cf360b5d31df4
isa <5445946+icortes@users.noreply.github.com>
2021-11-12 15:45:58 -0800

    added middleware to server.js to use

--- server.js
+++ server.js
@@ -3,3 +3,18 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3000;
+
+const db = require('./models');
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
