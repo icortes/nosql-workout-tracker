9894ca706c1343e24a91e232f9bfab39c8eb14b2
isa <5445946+icortes@users.noreply.github.com>
2021-11-12 20:27:28 -0800

    created workout model

---
+++ models/Workout.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  type: String,
+  name: String,
+  distance: Number,
+  weight: Number,
+  sets: Number,
+  reps: Number,
+  duration: Number,
+});
+
+const Workout = mongoose.model('Workout', WorkoutSchema);
+
+module.exports = Workout;
