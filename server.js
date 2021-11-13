bc892ce04a1681b9b1c5274228fd93229a4ebca0
isa <5445946+icortes@users.noreply.github.com>
2021-11-12 22:29:48 -0800

    fixed syntax error

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true }, { useUnifiedTopology: true });
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workoutdb',
+  {useNewUrlParser: true},
+  {useUnifiedTopology: true}
+);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
