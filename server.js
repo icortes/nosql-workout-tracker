fd4b484821045186d2df69ce2bb22ccda8a4db49
isa <5445946+icortes@users.noreply.github.com>
2021-11-12 22:47:10 -0800

    created index file and routes file

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const path = require('path');
+const routes = require('./routes');
 
 const PORT = process.env.PORT || 3000;
-
-const db = require('./models');
 
 const app = express();
 
@@ -13,13 +13,15 @@ app.use(logger('dev'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, './public')));
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/workoutdb',
   {useNewUrlParser: true},
   {useUnifiedTopology: true}
 );
+
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
