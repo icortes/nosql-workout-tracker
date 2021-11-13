7c559f26047a68d3fc6495a1ae2a67900419a5e2
isa <5445946+icortes@users.noreply.github.com>
2021-11-13 12:40:48 -0800

    created api router route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const routes = require('./routes');
+const apiRoutes = require('./api');
 
 router.use('/', routes);
+rputer.use('/api', apiRoutes);
 
 module.exports = router;
